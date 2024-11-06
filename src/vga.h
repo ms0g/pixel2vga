1b29e90db5db49c832e3cb35f62bb6d5d3926442
M. Sami Gürpınar <sami.gurpinar@gmail.com>
2024-11-06 18:14:21 +0300

    refactor(vga): header guard

--- src/vga.h
+++ src/vga.h
@@ -1,4 +1,5 @@
-#pragma once
+#ifndef VGA_H
+#define VGA_H
 
 #include <stdint.h>
 
@@ -7,3 +8,5 @@ typedef struct color {
 } color;
 
 const color vgaclamp(uint8_t r, uint8_t g, uint8_t b);
+
+#endif
